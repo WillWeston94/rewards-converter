fedde9544af8a5167f700157338877da3f78f471
Will Weston <Williamweston81@gmail.com>
2023-12-05 09:37:25 -0600

    Weston(3) Create: miles value constructor Create: getMilesValue method Create: getCashValue method

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,6 +1,6 @@
 public class RewardValue {
     private static final double MILES_TO_CASH_CONVERSION = 0.0035;
-    private double cashVlaue;
+    private double cashValue;
     private double milesValue;
 
     public RewardValue(double cashValue) {
@@ -8,5 +8,16 @@ public class RewardValue {
         this.milesValue = cashValue / MILES_TO_CASH_CONVERSION;
     }
 
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * MILES_TO_CASH_CONVERSION;
+    }
 
+    public double getCashValue(){
+        return cashValue;
+    }
+
+    public double getMilesValue(){
+        return milesValue;
+    }
 }
