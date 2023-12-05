0a1918967c913204e52dcffaadcad74408a70423
Will Weston <Williamweston81@gmail.com>
2023-12-05 09:28:35 -0600

    Create cash value constructor

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,12 @@
 public class RewardValue {
+    private static final double MILES_TO_CASH_CONVERSION = 0.0035;
+    private double cashVlaue;
+    private double milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_CONVERSION;
+    }
+
+
 }
